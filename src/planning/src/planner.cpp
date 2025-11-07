02a5061af7511bd74c614368e1d92b20bde8c717
Ian Shi <ianshi1026@gmail.com>
2025-11-06 22:46:15 -0500

    control position with controller

--- src/planning/src/planner.cpp
+++ src/planning/src/planner.cpp
@@ -20,7 +20,7 @@ void Planner::initialize_move_group() {
     move_group_->setMaxVelocityScalingFactor(0.8);
     move_group_->setMaxAccelerationScalingFactor(0.8);    
 
-    move_group_->setGoalPositionTolerance(0.05);
+    move_group_->setGoalPositionTolerance(0.01);
     move_group_->setGoalOrientationTolerance(3.14); // Allow any orientation
 
     RCLCPP_INFO(get_logger(), "Move group '%s' initialized with end effector link '%s'.",
@@ -28,17 +28,6 @@ void Planner::initialize_move_group() {
 }
 
 void Planner::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
-    if (msg->buttons[0] == 1) { // X button
-        geometry_msgs::msg::PoseStamped target_pose = move_group_->getRandomPose();
-        move_group_->setPoseTarget(target_pose);
-        moveit::planning_interface::MoveGroupInterface::Plan plan;
-        bool success = (move_group_->plan(plan) == moveit::core::MoveItErrorCode::SUCCESS);
-        if (success) {
-            move_group_->execute(plan);
-        } else {
-            RCLCPP_WARN(get_logger(), "Failed to find valid random plan.");
-        }
-    }
     // for (float x = 0.1; x < 0.5; x += 0.05) {
     //     for (float y = 0.1; y < 0.5; y += 0.05) {
     //         for (float z = 0.0; z < 0.5; z += 0.05) {
@@ -56,39 +45,31 @@ void Planner::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg) {
     //     }
     // }
 
-    // if (msg->buttons[0] == 1) { // X button
-    //     current_target_pose_.position.x -= 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // } else if (msg->buttons[1] == 1) { // A button
-    //     current_target_pose_.position.y -= 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // } else if (msg->buttons[2] == 1) { // B button
-    //     current_target_pose_.position.x += 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // } else if (msg->buttons[3] == 1) { // Y button
-    //     current_target_pose_.position.y += 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // } else if (msg->axes[5] < -0.5) { // D pad down
-    //     current_target_pose_.position.z -= 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // } else if (msg->axes[5] > 0.5) { // D pad up
-    //     current_target_pose_.position.z += 0.025;
-    //     plan_and_execute_to_target(current_target_pose_);
-    //     return;
-    // }
-
-    // for (size_t i = 0; i < msg->buttons.size(); i++) {
-    //     if (msg->buttons[i] == 1) {
-    //         RCLCPP_INFO(get_logger(), "Joystick button %zu pressed, moving to '%s' position.", i, button_to_position[i].c_str());
-    //         plan_and_execute_to_predefined_position(button_to_position[i]);
-    //         return;
-    //     }
-    // }
+    if (msg->buttons[0] == 1) { // X button
+        current_target_pose_.position.x -= 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    } else if (msg->buttons[1] == 1) { // A button
+        current_target_pose_.position.y -= 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    } else if (msg->buttons[2] == 1) { // B button
+        current_target_pose_.position.x += 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    } else if (msg->buttons[3] == 1) { // Y button
+        current_target_pose_.position.y += 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    } else if (msg->axes[5] < -0.5) { // D pad down
+        current_target_pose_.position.z -= 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    } else if (msg->axes[5] > 0.5) { // D pad up
+        current_target_pose_.position.z += 0.025;
+        plan_and_execute_to_target(current_target_pose_);
+        return;
+    }
 }
 
 bool Planner::plan_and_execute_to_target(geometry_msgs::msg::Pose target_pose) {
@@ -130,13 +111,6 @@ int main(int argc, char * argv[]) {
     rclcpp::init(argc, argv);
     auto planner = std::make_shared<Planner>();
     planner->initialize_move_group();
-
-    geometry_msgs::msg::Pose target_pose;
-    target_pose.position.x = 0.3;
-    target_pose.position.y = 0.0;
-    target_pose.position.z = 0.2;
-    planner->plan_and_execute_to_target(target_pose);
-
     rclcpp::spin(planner);
     rclcpp::shutdown();
     return 0;
